595216315d96b0fd8e5d947e690c05b86cce0980
lee-eojin <lejin0906@snu.ac.kr>
2026-06-04 16:05:55 +0900

    test: 가격 포멧팅 테스트 구현

---
+++ client/src/shared/lib/format.test.ts
@@ -0,0 +1,11 @@
+import { formatPrice } from "./format.ts";
+
+describe("formatPrice", () => {
+    test("천 단위 comma와 원 단위를 붙인다", () => {
+        expect(formatPrice(1000)).toBe("1,000원");
+    });
+
+    test("0원도 처리한다", () => {
+        expect(formatPrice(0)).toBe("0원");
+    });
+});
