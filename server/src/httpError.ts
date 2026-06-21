adf7d530dda6b0e1b02e8e4ac46fdbe4af1d4264
lee-eojin <lejin0906@snu.ac.kr>
2026-06-21 22:09:00 +0900

    refactor: 클라이언트가 분기할 수 있는 안정적인 식별자 추가

--- server/src/httpError.ts
+++ server/src/httpError.ts
@@ -4,6 +4,8 @@ export class HttpError extends Error {
   constructor(
     public readonly status: number,
     message: string,
+    public readonly code?: string,
+
   ) {
     super(message);
     this.name = "HttpError";
